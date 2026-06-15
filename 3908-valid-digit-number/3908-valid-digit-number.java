1d7f4e70e7c3fedf2dd275de30c7a988cb9e33ed
Varun Sharma <varunsharma30.vs@gmail.com>
2026-06-15 21:46:51 +0530

    Time: 1 ms (99.01%), Space: 42.5 MB (88.78%) - LeetHub

--- 3908-valid-digit-number/3908-valid-digit-number.java
+++ 3908-valid-digit-number/3908-valid-digit-number.java
@@ -1,22 +1,22 @@
 class Solution {
     public boolean validDigit(int n, int x) {
         int first = n;
-
-        while (first >= 10) {
+        while (first >= 10) 
+        {
             first /= 10;
         }
-
-        if (first == x) {
+        if (first == x) 
+        {
             return false;
         }
-
-        while (n > 0) {
-            if (n % 10 == x) {
+        while (n > 0) 
+        {
+            if (n % 10 == x) 
+            {
                 return true;
             }
             n /= 10;
         }
-
         return false;
     }
 }
