7701cdd802ec2755bac97673b1062bdd734aa5d7
Varun Sharma <varunsharma30.vs@gmail.com>
2026-06-20 10:12:40 +0530

    Time: 71 ms (8.87%), Space: 41.6 MB (96.37%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,5 +1,9 @@
 class Solution {
     public int fib(int n) {
+        return solve(n);
+    }
+    int solve(int n)
+    {
         if(n==0)
         {
             return 0;
@@ -8,6 +12,6 @@ class Solution {
         {
             return 1;
         }
-        return fib(n-1)+fib(n-2);
+        return solve(n-1)+solve(n-2);
     }
 }
